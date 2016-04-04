7ba78c2e4dd7e95ba32e8699c07f82441bb69b76
hokagedemehin <ibk2k7@gmail.com>
2016-04-04 14:47:39 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,17 @@
 plot2<-function(){
   
   data<-read.csv('household_power_consumption.txt', stringsAsFactors = FALSE,sep=';')
-  data[,1]<-as.Date(data[,1], "%d/%m/%Y")
-  data<-subset(data,Date=="2007-02-01"|Date=="2007-02-02")
+  
+  data<-subset(data,Date=="1/2/2007"|Date=="2/2/2007")
+  datetime<-paste(data$Date,data$Time, sep = " ")
+  data$Date<-strptime(datetime, "%d/%m/%Y %X")
+  
   for(i in 3:9){
     data[,i]<-as.numeric(data[,i])
   }
-  png("plot2.png", width=480, height=480)
-  c<-plot(data$Global_active_power,  type = 'n',xlab = "",
+  c<-plot(data$Date,data$Global_active_power,  type = 'n',xlab = "",
        ylab = "Global Active Power(kilowatts)")
-  lines(data$Global_active_power,lty=1)
+  lines(data$Date,data$Global_active_power,lty=1)
+  dev.copy(png, file='plot2.png')
   dev.off()
 }
