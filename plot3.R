8ea537e835312eb3d5d29413225a0726e8379ede
hokagedemehin <ibk2k7@gmail.com>
2016-04-04 02:01:53 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,6 +6,7 @@ plot3<-function(){
   for(i in 3:9){
     data[,i]<-as.numeric(data[,i])
   }
+  png("plot3.png", width=480, height=480)
   plot(data$Sub_metering_1,  type = 'n',xlab = "",
        ylab = "Energy sub metering")
   lines(data$Sub_metering_1)
@@ -15,5 +16,5 @@ plot3<-function(){
   text(2500,38,lty=1 ,label= '- Sub_metering_1')
   text(2500,34,lty=1 , col = 'red',label= '- Sub_metering_2')
   text(2500,30,lty=1 , col = 'blue',label= '- Sub_metering_3')
-  
+  dev.off()
   }
