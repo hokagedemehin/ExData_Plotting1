9b05e8004057aee2af83360b54d7cf89a50f40c6
hokagedemehin <ibk2k7@gmail.com>
2016-04-04 14:44:09 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -11,6 +11,6 @@ plot1<-function(){
   }
   hist(data$Global_active_power,xlab = "Global Active Power(kilowatts)",
        col='red',main='Global Active Power')
-  dev.copy(png, file='pp.png')
+  dev.copy(png, file='plot1.png')
   dev.off()
 }
